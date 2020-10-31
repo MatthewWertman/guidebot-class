b6b547980219553d4e39727a1ab489b0a9cc644e
MatthewWertman <mattwert@charter.net>
2020-10-31 12:31:13 -0700

    Update & reformat events

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,11 @@
-// This event executes when a new guild (server) is joined.
-
 module.exports = class {
-  constructor (client) {
-    this.client = client;
-  }
+    constructor (client) {
+        this.client = client;
+    }
 
-  async run (guild) {
+    async run (guild) {
 
-    this.client.user.setActivity(`${this.client.settings.get("default").prefix}help | ${this.client.guilds.cache.size} Servers`);
-    this.client.logger.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`);
-  }
+        this.client.user.setActivity(`${this.client.config.botSettings.prefix}help | ${this.client.guilds.cache.size} Servers`);
+        console.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`);
+    }
 };
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,12 @@
-// This event executes when a new member joins a server. Let's welcome them!
-
 module.exports = class {
-  constructor (client) {
-    this.client = client;
-  }
+    constructor (client) {
+        this.client = client;
+    }
 
-  async run (member) {
-    
-    // Load the guild's settings
-    const settings = this.client.getSettings(member.guild);
-  
-    // If welcome is off, don't proceed (don't welcome the user)
-    if (settings.welcomeEnabled !== "true") return;
+    async run (member) {
+        if (this.client.config.botSettings.welcomeEnabled !== "true") return;
+        const welcomeMessage = this.client.config.botSettings.welcomeMessage.replace("{{user}}", member.user.tag);
 
-    // Replace the placeholders in the welcome message with actual data
-    const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
-
-    // Send the welcome message to the welcome channel
-    // There's a place for more configs here.
-    member.guild.channels.cache.find(c => c.name === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
-  }
+        member.guild.channels.cache.find(c => c.name === this.client.config.botSettings.welcomeChannel).send(welcomeMessage).catch(console.error);
+    }
 };
