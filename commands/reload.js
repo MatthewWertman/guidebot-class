c59e185c473a98919636c064733bcc7fbd893aa4
MatthewWertman <mattwert@charter.net>
2020-10-31 11:56:29 -0700

    Update & reformat commands

--- commands/reload.js
+++ commands/reload.js
@@ -1,29 +1,38 @@
 const Command = require("../base/Command.js");
 
 class Reload extends Command {
-  constructor (client) {
-    super(client, {
-      name: "reload",
-      description: "Reloads a command that has been modified.",
-      category: "System",
-      usage: "reload [command]",
-      permLevel: "Bot Admin"
-    });
-  }
+    constructor (client) {
+        super(client, {
+            name: "reload",
+            description: "Reloads a command that has been modified.",
+            category: "System",
+            usage: "reload [command]",
+            permLevel: "Bot Admin"
+        });
+    }
 
-  async run (message, args, level) { // eslint-disable-line no-unused-vars
-    if (!args || args.size < 1) return message.reply("Must provide a command to reload. Derp.");
-    
-    const commands = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
-    if (!commands) return message.reply(`The command \`${args[0]}\` does not exist, nor is it an alias.`);
-
-    let response = await this.client.unloadCommand(commands.conf.location, commands.help.name);
-    if (response) return message.reply(`Error Unloading: ${response}`);
-
-    response = this.client.loadCommand(commands.conf.location, commands.help.name);
-    if (response) return message.reply(`Error loading: ${response}`);
-
-    message.reply(`The command \`${commands.help.name}\` has been reloaded`);
-  }
+    async run (message, args, level) { // eslint-disable-line no-unused-vars
+        let command;
+        if (!args || args.size < 1) return message.reply("Must provide a command to reload.");
+        if (this.client.commands.has(args[0])) {
+            command = args[0];
+        } else if (this.client.aliases.has(args[0])) {
+            command = this.client.aliases.get(args[0]);
+        }
+        if (!command) {
+            return message.channel.send(`I cannot find the command: ${args[0]}`);
+        } else {
+            message.channel.send(`Reloading ${command}`)
+                .then(m => {
+                    this.client.reload(command)
+                        .then(() => {
+                            m.edit(`Successfully reloaded: ${command}`);
+                        })
+                        .catch(e => {
+                            m.edit(`Command reload failed: ${command}\n\`\`\`${e.stack}\`\`\``);
+                        });
+                });
+        }
+    }
 }
 module.exports = Reload;
--- commands/stats.js
+++ commands/stats.js
@@ -1,28 +1,28 @@
 const Command = require("../base/Command.js");
-const { version } = require("discord.js");
+const {
+    version,
+    MessageEmbed
+} = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
 
 class Stats extends Command {
-  constructor (client) {
-    super(client, {
-      name: "stats",
-      description: "Gives some useful bot statistics.",
-      usage: "stats",
-    });
-  }
+    constructor (client) {
+        super(client, {
+            name: "stats",
+            description: "Gives some useful bot statistics.",
+            usage: "stats",
+        });
+    }
 
-  async run (message, args, level) { // eslint-disable-line no-unused-vars
-    const duration = moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-    message.channel.send(`= STATISTICS =
-  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Uptime     :: ${duration}
-  • Users      :: ${this.client.users.cache.size.toLocaleString()}
-  • Servers    :: ${this.client.guilds.cache.size.toLocaleString()}
-  • Channels   :: ${this.client.channels.cache.size.toLocaleString()}
-  • Discord.js :: v${version}
-  • Node       :: ${process.version}`, {code: "asciidoc"});
-  }
+    async run (message, args, level) { // eslint-disable-line no-unused-vars
+        const duration = moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        const embed = new MessageEmbed()
+            .setColor(0x00ff00)
+            .setTitle("STATISTICS")
+            .addField("🤓", `• Mem Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n• Uptime: ${duration}\n• Users: ${this.client.users.cache.size.toLocaleString()}\n• Servers: ${this.client.guilds.cache.size.toLocaleString()}\n• Channels: ${this.client.channels.cache.size.toLocaleString()}\n• Discord.js: v${version}\n• Node: ${process.version}`);
+        message.channel.send(embed);
+    }
 }
 
 module.exports = Stats;
