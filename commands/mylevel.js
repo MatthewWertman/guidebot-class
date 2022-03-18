bfb3fd3be9bcdc45dd7835c091c253d9af5ec8d6
MattthewWertman <matthewwertman57@gmail.com>
2022-03-17 22:04:15 -0700

    add mylevel as global slash command

--- commands/mylevel.js
+++ commands/mylevel.js
@@ -1,10 +1,15 @@
 const Command = require("../base/Command.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 class MyLevel extends Command {
     constructor (client) {
         super(client, {
             name: "mylevel",
             description: "Displays your permission level for your location.",
+            data: new SlashCommandBuilder()
+                .setName("mylevel")
+                .setDescription("Displays your permission level for your location."),
+            slashEnable: true,
             usage: "mylevel",
             guildOnly: true
         });
@@ -13,6 +18,12 @@ class MyLevel extends Command {
     async run (message, args, level) {
         const friendly = this.client.config.permLevels.find(l => l.level === level).name;
         message.reply(`Your permission level is: ${level} - ${friendly}`);
+    }
+    
+    async interact (interaction) {
+        const level = this.client.permlevel(interaction);
+        const lvlName = this.client.config.permLevels.find(l => l.level === level).name;
+        interaction.reply(`Your permission level is: ${level} - ${lvlName}`);
     }
 }
 
