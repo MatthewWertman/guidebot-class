1e7d5955ddc9cffd97e0397509ed4c73879fd49f
MattthewWertman <matthewwertman57@gmail.com>
2022-03-21 11:30:28 -0700

    add image arg, defaults to annoucements channel

--- commands/poll.js
+++ commands/poll.js
@@ -6,7 +6,7 @@ class Poll extends Command {
         super(client, {
             name: "poll",
             description: "Creates a yes-no poll for people to vote on",
-            usage: "poll <question>",
+            usage: "poll [image link] <question>",
             guildOnly: true,
             permLevel: "Server Owner"
         });
@@ -14,19 +14,27 @@ class Poll extends Command {
 
     async run (message, args, level) { //eslint-disable-line no-unused-vars
         if (!args[0]) return message.channel.send(`USAGE: ${this.help.usage}`);
-        const channel = message.member.guild.channels.cache.find(ch => ch.name === "announcements");
-        if (!channel) return;
+        let channel = message.member.guild.channels.cache.find(ch => ch.name === "announcements");
+        if (!channel || !message.guild.members.cache.find(m => m.id === message.guild.me.id).permissionsIn(channel).has("SEND_MESSAGES")) channel = message.channel;
+        let title = args.join(" ");
+        let image;
+        const linkCheck = /https?:\/\/.+\.(?:png|jpg|jpeg)/gi;
+        if (args[0] && linkCheck.test(args[0])) {
+            title = args.slice(1, args.length).join(" ");
+            image = args[0];
+        }
         const embed = new MessageEmbed()
             .setColor(0xff0000)
             .setDescription("React with ✅ to vote yes\nReact with ❌ to vote no")
             .setFooter("React to vote", `${message.author.displayAvatarURL()}`)
-            .setTitle(`${args.join(" ")}`);
+            .setTitle(`${title}`);
+        if (image) {
+            embed.setImage(args[0]);
+        }
 
         const msg = await channel.send({embeds: [embed]});
         await msg.react("✅");
         await msg.react("❌");
-
-        // Delete original message
     }
 }
 
