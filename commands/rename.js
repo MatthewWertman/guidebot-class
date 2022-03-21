d3fe6cfd8bf69682a9afc275919130dfeba7e13a
MattthewWertman <matthewwertman57@gmail.com>
2022-03-21 09:55:34 -0700

    clean up rename command

--- commands/rename.js
+++ commands/rename.js
@@ -7,7 +7,7 @@ class Rename extends Command {
             description: "Changes bot nickname",
             category: "Config",
             guildOnly: true,
-            usage: "rename <nickname>",
+            usage: "rename <nickname>|reset",
             aliases: ["name"],
             permLevel: "Server Owner"
         });
@@ -15,16 +15,18 @@ class Rename extends Command {
 
     async run (message, args, level) { //eslint-disable-line no-unused-vars
         if (!args[0] && args.length === 0) return message.channel.send(`USAGE: ${this.help.usage}`);
-        let name = await this.client.clean(args.slice(0, args.length).join(" "));
+        let name = await this.client.clean(args.join(" "));
         if (name.length > 32) name = name.substring(0, 32);
-        if (name == "reset") {
-            message.guild.me.setNickname(null);
-            message.reply("Successfully reverted nickname!");
-            this.client.logger.log(`${message.author.username} (${message.author.id}) removed nickname from bot.`);
-        } else {
-            message.guild.me.setNickname(name);
-            message.reply(`Successfully changed name to '${name}'`);
-            this.client.logger.log(`${message.author.username} (${message.author.id}) renamed the bot to '${name}'.`);
+        switch (name) {
+            case "reset":
+                message.guild.me.setNickname(null);
+                message.reply("Successfully reverted nickname!");
+                this.client.logger.log(`${message.author.username} (${message.author.id}) removed nickname from bot.`);
+                break;
+            default:
+                message.guild.me.setNickname(name);
+                message.reply(`Successfully changed name to '${name}'`);
+                this.client.logger.log(`${message.author.username} (${message.author.id}) renamed the bot to '${name}'.`);
         }
     }
 }
