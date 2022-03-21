af065f294d090fcd145c665adca0cfbfc42f58c9
MattthewWertman <matthewwertman57@gmail.com>
2022-03-21 00:19:18 -0700

    add rename command to set nickname for bot

---
+++ commands/rename.js
@@ -0,0 +1,32 @@
+const Command = require("../base/Command.js");
+
+class Rename extends Command {
+    constructor (client) {
+        super(client, {
+            name: "rename",
+            description: "Changes bot nickname",
+            category: "Config",
+            guildOnly: true,
+            usage: "rename <nickname>",
+            aliases: ["name"],
+            permLevel: "Server Owner"
+        });
+    }
+
+    async run (message, args, level) { //eslint-disable-line no-unused-vars
+        if (!args[0] && args.length === 0) return message.channel.send(`USAGE: ${this.help.usage}`);
+        let name = await this.client.clean(args.slice(0, args.length).join(" "));
+        if (name.length > 32) name = name.substring(0, 32);
+        if (name == "reset") {
+            message.guild.me.setNickname(null);
+            message.reply("Successfully reverted nickname!");
+            this.client.logger.log(`${message.author.username} (${message.author.id}) removed nickname from bot.`);
+        } else {
+            message.guild.me.setNickname(name);
+            message.reply(`Successfully changed name to '${name}'`);
+            this.client.logger.log(`${message.author.username} (${message.author.id}) renamed the bot to '${name}'.`);
+        }
+    }
+}
+
+module.exports = Rename;
