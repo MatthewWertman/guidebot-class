aebaf3c9377c4e3173ea183a35339a96b2e6b6c6
MattthewWertman <matthewwertman57@gmail.com>
2022-10-12 22:28:46 -0700

    use api v10

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { REST, Routes } = require("discord.js");
 
 module.exports = class {
     constructor (client) {
@@ -14,7 +13,7 @@ module.exports = class {
         await this.client.wait(1000);
 
         if (this.client.config.botSettings.updateGlobalSlashes === "true") {
-            const rest = new REST({ version: "9" }).setToken(this.client.config.token);
+            const rest = new REST({ version: "10" }).setToken(this.client.config.token);
 
             try {
                 this.client.logger.log("Started refreshing application (/) commands.");
